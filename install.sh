f09d4916a55f5f0945f7b67e7dd3640c2655b1f9
kei <keitargz@giahub.com>
2024-10-02 10:17:05 +0900

    first commit

---
+++ install.sh
@@ -0,0 +1,21 @@
+sudo apt-get install wget gpg
+# VSCode repository settings
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg ./config/archives/vscode.key.chroot
+cp ./config/archives/vscode.key.chroot ./config/archives/vscode.key.binary
+rm -f packages.microsoft.gpg
+
+# Chrome repository settings
+wget -q -O https://dl-ssl.google.com/linux/linux/signing_key.pub > config/archives/chrome.key.chroot
+cp ./config/archives/chrome.key.chroot ./config/archives/chrome.key.binary
+
+# config
+lb config \
+		--apt apt \
+		--architecture amd64 \
+		--distribution stable \
+		--parent-archive-areas "main contrib non-free non-free-firmware" \
+		--mirror-bootstrap "http://deb.debian.org/debian" \
+		--mirror-chroot "http://deb.debian.org/debian" \
+		--bootappend-live "boot=live components locales=ja_JP.UTF-8 timezone=Asia/Tokyo keyboard-layouts=jp debug=1" \
+		--binary-image iso \
