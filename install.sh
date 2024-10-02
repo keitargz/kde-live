d7f55269a797be4a79af84ed475537fe81a90ca7
kei <keitargz@giahub.com>
2024-10-02 14:06:32 +0900

    added vscode & chrome

--- install.sh
+++ install.sh
@@ -1,13 +1,6 @@
-sudo apt-get install wget gpg
-# VSCode repository settings
-wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-sudo install -D -o root -g root -m 644 packages.microsoft.gpg ./config/archives/vscode.key.chroot
-cp ./config/archives/vscode.key.chroot ./config/archives/vscode.key.binary
-rm -f packages.microsoft.gpg
+#!/bin/bash
 
-# Chrome repository settings
-wget -qO- https://dl-ssl.google.com/linux/Linux_signing_key.pub > config/archives/chrome.key.chroot
-cp ./config/archives/chrome.key.chroot ./config/archives/chrome.key.binary
+set -e
 
 # config
 lb config \
@@ -19,3 +12,4 @@ lb config \
 		--mirror-chroot "http://deb.debian.org/debian" \
 		--bootappend-live "boot=live components locales=ja_JP.UTF-8 timezone=Asia/Tokyo keyboard-layouts=jp debug=1" \
 		--binary-image iso \
+
